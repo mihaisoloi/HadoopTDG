b0a30d320e41b15d521b372b59ca7a8e983dfe3c
Mihai Soloi <mihai.soloi@gmail.com>
2012-07-10 22:51:22 +0300

    no class def exception on minidfscluster

--- src/test/java/org/hadoop/tdg/TestPseudoHadoop.java
+++ src/test/java/org/hadoop/tdg/TestPseudoHadoop.java
@@ -6,9 +6,10 @@ import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hdfs.MiniDFSCluster;
 import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.util.Progressable;
-import org.junit.Test;
+import org.junit.*;
 
 import java.io.*;
 import java.net.URI;
@@ -32,24 +33,62 @@ import java.net.URL;
  */
 public class TestPseudoHadoop {
 
-    private static final String DST = "hdfs://localhost/user/"+System.getProperty("user.name");
+    private static final String DST = "/user/" + System.getProperty("user.name");
     private static final String HOME = System.getProperty("user.home");
-    private static final String DST_FILE = DST + "/test";
+    private static final String DST_FILE = "/dir/test";
     private static final String HOME_FILE = HOME + "/test";
-    private static final Configuration CONF = new Configuration();
+    private Configuration conf;
+    private MiniDFSCluster cluster;
+    private FileSystem fs;
 
-    static { // only once per JVM
+    @BeforeClass
+    public static void setUpClass() throws IOException {
         RandomAccessFile f = null;
         try {
-            f = new RandomAccessFile(HOME + "/test", "rw");
-            f.setLength(4096*1000);
-        } catch (IOException e) {
-            //won't happen
+            f = new RandomAccessFile(HOME_FILE, "rw");
+            f.setLength(4096 * 1000);
         } finally {
             IOUtils.closeStream(f);
         }
-
         URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+    }
+
+    @Before
+    public void setUp() throws IOException {
+        conf = new Configuration();
+        if (System.getProperty("test.build.data") == null) {
+            System.setProperty("test.build.data", "/tmp");
+        }
+        cluster = new MiniDFSCluster(conf, 1, true, null);
+        fs = cluster.getFileSystem();
+        copyFileWithProgress();
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        if (fs != null) fs.close();
+        if (cluster != null) cluster.shutdown();
+    }
+
+    @Ignore
+    public void copyFileWithProgress() throws IOException {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = new BufferedInputStream(new FileInputStream(HOME_FILE));
+//            FileSystem fs = FileSystem.get(URI.create(DST), conf);
+            out = fs.create(new Path(DST_FILE), new Progressable() {
+                @Override
+                public void progress() {
+                    System.out.print("~");
+                }
+            });
+
+            IOUtils.copyBytes(in, out, 4096, true);
+        } finally {
+            IOUtils.closeStream(in);
+            IOUtils.closeStream(out);
+        }
     }
 
     @Test
@@ -59,35 +98,23 @@ public class TestPseudoHadoop {
 
     @Test
     public void readWithFileSystem() throws IOException {
-        FileSystem fs = FileSystem.get(URI.create(DST_FILE), CONF);
+//        FileSystem fs = FileSystem.get(URI.create(DST_FILE), conf);
         FSDataInputStream is = fs.open(new Path(DST_FILE));
         is.seek(23);
         printStream(is);
     }
 
-    @Test
-    public void copyFileWithProgress() throws IOException {
-        InputStream in = new BufferedInputStream(new FileInputStream(HOME_FILE));
-        FileSystem fs = FileSystem.get(URI.create(DST), CONF);
-        OutputStream out = fs.create(new Path(DST_FILE), new Progressable() {
-            @Override
-            public void progress() {
-                System.out.print("~");
-            }
-        });
-
-        IOUtils.copyBytes(in, out, 4096, true);
-    }
-
     private void printStream(InputStream is) throws IOException {
         File f1 = new File(HOME_FILE);
-        File f2 = new File(HOME+"/test.cpy");
-        FileOutputStream fos = new FileOutputStream(f2);
+        File f2 = new File(HOME + "/test.cpy");
+        FileOutputStream fos = null;
         try {
+            fos = new FileOutputStream(f2);
             IOUtils.copyBytes(is, fos, 4096, false);
-            Files.equal(f1,f2);
+            Files.equal(f1, f2);
         } finally {
             IOUtils.closeStream(is);
+            IOUtils.closeStream(fos);
         }
     }
 
